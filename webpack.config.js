db0610f1baff644e94049a7beb473b2e7064182a
VladimirNeri <vlad.neri@gmail.com>
2020-09-22 10:02:38 -0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
       },
       {
         test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.md$/i,
