03c2085453e61d51ec28efee59a775ac34ce39fc
VladimirNeri <vlad.neri@gmail.com>
2020-09-23 12:21:53 -0700

    hero

--- client/src/components/Hero/hero.style.js
+++ client/src/components/Hero/hero.style.js
@@ -4,10 +4,18 @@ export const HeroStyl = styled.div`
   margin: auto; 
   width: 70%; 
   padding-bottom: 10px; 
-
+  
 
   @media (max-width: 450px) {
     width: 100%
+  }
+
+  h3 {
+    font-size: 5em; 
+  }
+
+  p {
+    font-size: 2em;
   }
 
   /* Should be on the markup */
