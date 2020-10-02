caf81ed60b6e42d168efa6f0ad8f27536d61003f
VladimirNeri <vlad.neri@gmail.com>
2020-10-01 19:34:54 -0700

    issues

--- controllers/subController.js
+++ controllers/subController.js
@@ -1,19 +1,4 @@
 const Subscriber = require('../models/submodel');
-// const db = require('../models');
-
-// module.exports = {
-//   create: function (req, res) {
-//     db.Subscriber.create(req.body)
-//       .then((dbSub) => res.json(dbSub))
-//       .catch((err) => res.status(422).json(err));
-//   },
-// };
-
-// exports.create = (req, res) => {
-//   db.Subscriber.create(req.body)
-//     .then((dbSub) => res.json(dbSub))
-//     .catch((err) => res.status(422).json(err));
-// };
 
 exports.create = async (req, res) => {
   try {
