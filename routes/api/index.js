3320a3652bde09bb582833c3aa20a92619ad6aeb
VladimirNeri <vlad.neri@gmail.com>
2020-10-02 21:11:13 -0700

    closed server

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const router = require('express').Router();
 const subRoutes = require('./sub');
+const postRoutes = require('./post'); 
 
 // API Routes
 router.use('/sub', subRoutes);
+router.use('/post', postRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function (req, res) {
---
+++ routes/api/post.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const postController = require('../../controllers/postcontroller');
+
+router
+  .route('/')
+  .post(postController.getPosts);
+
+module.exports = router;
