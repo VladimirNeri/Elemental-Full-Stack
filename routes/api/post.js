09696f8e1bc3560d73731d54f2f69901c67e687b
VladimirNeri <vlad.neri@gmail.com>
2020-10-02 23:22:29 -0700

    disabled routes

--- routes/api/post.js
+++ routes/api/post.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../../controllers/postcontroller');
 
-router
-  .route('/')
-  .post(postController.getPosts);
+// router
+//   .route('/')
+//   .post(postController.getPosts);
 
 module.exports = router;
