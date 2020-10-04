d484ab07a6644fe411665f922c217cfb90d24e45
VladimirNeri <vlad.neri@gmail.com>
2020-10-04 02:10:15 -0700

    server

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+
+app.use(express.static('client/build'));
+
 
 // Add routes, both API and view
 app.use(routes);
