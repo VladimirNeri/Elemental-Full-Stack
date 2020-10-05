f4cd56dc6e1fe9d418920a2fbf5c6a0f44df2c1e
VladimirNeri <vlad.neri@gmail.com>
2020-10-05 16:18:08 -0700

    .

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.use('/api/sub', subRoutes);
+app.use('/sub', subRoutes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default {
   createSub: function(subscribers) {
-    return axios.post('/api/sub', subscribers);
+    return axios.post('/sub', subscribers);
   }
 }
