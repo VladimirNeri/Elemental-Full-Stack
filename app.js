04bf0857f190c1578d7b378148609f7fb390bc1a
VladimirNeri <vlad.neri@gmail.com>
2020-10-05 15:27:36 -0700

    .

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-
 const subRoutes = require('./routes/sub');
 
 // Configure body parsing for AJAX requests
--- routes/sub.js
+++ routes/sub.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const subController = require('../controllers/subcontroller');
-
 const router = express.Router();
+const subController = require('../controllers/subcontroller');
 
 router.route('/').post(subController.create);
 
