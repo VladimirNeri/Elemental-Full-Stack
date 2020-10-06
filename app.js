407bdb7d418530425f20415547a10be4a08b5351
VladimirNeri <vlad.neri@gmail.com>
2020-10-05 17:06:24 -0700

    .

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.use('/sub', subRoutes);
+app.use('/api/sub', subRoutes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
