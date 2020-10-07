695f23df8e213d6c8d1665abd7e54d15dbbf89af
VladimirNeri <vlad.neri@gmail.com>
2020-10-07 08:59:14 -0700

    router

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.use('/api', routes, function (req, res, next) {
+app.use('/api', routes, function (err, req, res, next) {
   if (err) {
     res.status(500).send({ error: 'router not mounted in heroku'})
   } else {
