c7d4add4c23b2364208e414526b041e88b45126e
VladimirNeri <vlad.neri@gmail.com>
2020-10-07 10:15:04 -0700

    routes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const router = express.Router()
-const routes = require('./routes');
+// const routes = require('./routes');
 const path = require('path'); 
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+const Subscriber = require('./models/submodel');
 
-const subController = require('./controllers/subcontroller');
+// const subController = require('./controllers/subcontroller');
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
@@ -29,9 +30,24 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.use('/api/sub', router)
+app.use('/api', router)
 
-router.post(subController.create);
+router.post('/sub', async (req, res) => {
+  try {
+    const Subscribers = await Subscriber.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        Subscribers
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+});
 
 app.use('/', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
