9f3adab3f79f47e0750949ce1ad357036f645f49
VladimirNeri <vlad.neri@gmail.com>
2020-11-11 01:16:51 -0800

    heroku

--- controllers/subController.js
+++ controllers/subController.js
@@ -1,16 +1,16 @@
-const Subscriber = require('../models/submodel');
-const catchAsync = require('./../utils/catchAsync');
+// const Subscriber = require('../models/submodel');
+// const catchAsync = require('./../utils/catchAsync');
 
-exports.create = catchAsync(async (req, res) => {
-  const Subscribers = await Subscriber.create({
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    email: req.body.email,
-  });
-  res.status(201).json({
-    status: 'success',
-    data: {
-      Subscribers,
-    },
-  });
-});
+// exports.create = catchAsync(async (req, res) => {
+//   const Subscribers = await Subscriber.create({
+//     firstname: req.body.firstname,
+//     lastname: req.body.lastname,
+//     email: req.body.email,
+//   });
+//   res.status(201).json({
+//     status: 'success',
+//     data: {
+//       Subscribers,
+//     },
+//   });
+// });
