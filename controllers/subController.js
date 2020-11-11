f922d01c0dee0669e8128a8c55eae051dcc8009a
VladimirNeri <vlad.neri@gmail.com>
2020-11-11 00:50:47 -0800

    subcontroller

--- controllers/subController.js
+++ controllers/subController.js
@@ -2,20 +2,15 @@ const Subscriber = require('../models/submodel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.create = catchAsync(async (req, res) => {
-  try {
-    const Subscribers = await Subscriber.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        Subscribers
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
+  const Subscribers = await Subscriber.create({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+  });
+  res.status(201).json({
+    status: 'success',
+    data: {
+      Subscribers,
+    },
+  });
 });
-
-
